f3ef64a2885502e7a34a226c60a585f4c78aaca8
Youssef Lagtab <yousseflagtab@gmail.com>
2020-03-25 21:16:54 +0100

    add ft_strdup

---
+++ ft_strdup.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ylagtab <ylagtab@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/31 23:44:28 by ylagtab           #+#    #+#             */
+/*   Updated: 2020/03/06 22:50:21 by ylagtab          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+    size_t  i;
+    size_t  len;
+	char    *ret;
+
+    len = 0;
+    while (s[len])
+        len++;
+	if ((ret = (char *)malloc(len + 1)) == NULL)
+		return (NULL);
+    while (s[i])
+    {
+        ret[i] = s[i];
+        i++;
+    }
+    return (ret);
+}
