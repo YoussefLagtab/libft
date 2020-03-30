876b53662d6a0dd0131389f6e27ca0fa12a9f162
Youssef Lagtab <yousseflagtab@gmail.com>
2020-03-30 15:21:34 +0100

    add ft_strequ

--- ft_strequ.c
+++ ft_strequ.c
@@ -14,10 +14,10 @@
 
 int		ft_strequ(char const *s1, char const *s2)
 {
-    int i;
+    int	i;
 
     if (s1 == NULL || s2 == NULL)
-		return (0);
+		return (-1);
 	i = 0;
 	while (s1[i] && s2[i] && s1[i] == s2[i])
 		i++;
