a2059efd73769023f2e13f677e3bb4b89123c798
Youssef Lagtab <yousseflagtab@gmail.com>
2020-03-30 14:24:54 +0100

    add ft_strlcat

---
+++ ft_strlcat.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ylagtab <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/01 17:31:26 by ylagtab           #+#    #+#             */
+/*   Updated: 2019/04/01 23:25:41 by ylagtab          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	j;
+	size_t	dst_len;
+	size_t	src_len;
+
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	if (size <= dst_len)
+		return (size + src_len);
+	else
+	{
+		i = dst_len;
+		j = 0;
+		size -= dst_len + 1;
+		while (j < size && src[j])
+			dst[i++] = src[j++];
+	}
+	return (dst_len + src_len);
+}
