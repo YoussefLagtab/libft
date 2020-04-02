89fb2d714a63740a5e5aef8e2aa57c2500c0f11c
Youssef Lagtab <yousseflagtab@gmail.com>
2020-04-02 02:40:14 +0100

    fix ft_strchr
    use casted vaariable `ch` instead of `c` in condition
    cast the returned address

--- ft_strchr.c
+++ ft_strchr.c
@@ -23,8 +23,8 @@ char	*ft_strchr(const char *s, int c)
     i = 0;
 	while (s[i])
     {
-		if (s[i] == c)
-			return (s + i);
+		if (s[i] == ch)
+			return ((char *)(s + i));
         i++;
     }
     return (NULL);
