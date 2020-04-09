3e9ccd2412173d7ddacca74d6af3f720116726ec
Youssef Lagtab <yousseflagtab@gmail.com>
2020-04-09 20:24:12 +0100

    cast to unsigned char

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -21,5 +21,5 @@ int		ft_strcmp(const char *s1, const char *s2)
 		return (-1);
 	while (s1[i] && s2[i] && s1[i] == s2[i])
 		i++;
-	return (s1[i] - s2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
