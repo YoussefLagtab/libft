d1514cd28a96849782ecd224af78b201db203fea
Youssef Lagtab <yousseflagtab@gmail.com>
2021-02-14 10:24:42 +0100

    fix ft_vector_add_at
    - rename new_array -> new_arr (norms :P)
    - modify capacity when create new array.

--- ft_vector_add.c
+++ ft_vector_add.c
@@ -6,7 +6,7 @@
 /*   By: ylagtab <ylagtab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/15 08:58:27 by ylagtab           #+#    #+#             */
-/*   Updated: 2021/02/13 15:38:47 by ylagtab          ###   ########.fr       */
+/*   Updated: 2021/02/14 10:24:24 by ylagtab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,28 +51,28 @@ int					ft_vector_add(t_vector *vector, void *content,
 int					ft_vector_add_at(t_vector *vector, size_t index,
 						void *content, size_t content_size)
 {
-	t_element	**new_array;
+	t_element	**new_arr;
 	t_element	*data;
 	size_t		size;
 
-	if (index >= vector->length)
+	if (index > vector->length)
 		return (1);
 	data = ft_element_new(content, content_size);
 	if (vector->length == vector->capacity)
 	{
-		size = sizeof(t_element*) * (vector->capacity * 2);
-		new_array = (t_element**)ft_malloc(size);
-		ft_memmove(new_array, vector->array, sizeof(t_element*) * index);
+		vector->capacity *= 2;
+		new_arr = (t_element**)ft_malloc(sizeof(t_element*) * vector->capacity);
+		ft_memmove(new_arr, vector->array, sizeof(t_element*) * index);
 	}
 	else
-		new_array = vector->array;
+		new_arr = vector->array;
 	size = sizeof(t_element*) * (vector->length - index);
-	ft_memmove(new_array + index + 1, vector->array + index, size);
-	new_array[index] = data;
-	if (new_array != vector->array)
+	ft_memmove(new_arr + index + 1, vector->array + index, size);
+	new_arr[index] = data;
+	if (new_arr != vector->array)
 	{
 		free(vector->array);
-		vector->array = new_array;
+		vector->array = new_arr;
 	}
 	vector->length = vector->length + 1;
 	return (0);
