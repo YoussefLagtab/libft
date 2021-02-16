f5662682d60b5ba9d34efc4607c538a7cd41ceb2
Youssef Lagtab <yousseflagtab@gmail.com>
2021-02-16 11:27:02 +0100

    protect delete function

--- ft_vector_remove_at.c
+++ ft_vector_remove_at.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_vector_remove_index.c                           :+:      :+:    :+:   */
+/*   ft_vector_remove_at.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ylagtab <ylagtab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/15 09:27:40 by ylagtab           #+#    #+#             */
-/*   Updated: 2021/02/13 12:01:57 by ylagtab          ###   ########.fr       */
+/*   Updated: 2021/02/16 11:18:01 by ylagtab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ int	ft_vector_remove_at(t_vector *vector, size_t index,
 	if (index >= vector->length)
 		return (1);
 	data = vector->array[index];
-	(*del)(data->content, data->content_size);
+	if (del != NULL)
+		(*del)(data->content, data->content_size);
 	free(data);
 	elements_size = sizeof(t_element*) * (vector->length - index - 1);
 	ft_memmove(vector->array + index, vector->array + index + 1, elements_size);
