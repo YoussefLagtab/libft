49b5a663f99a33432aaa2ccd885e7be9ee69cafe
Youssef Lagtab <yousseflagtab@gmail.com>
2021-02-20 16:40:59 +0100

    old_size must be the length of the vector not the capacity :(

--- ft_vector_realloc.c
+++ ft_vector_realloc.c
@@ -6,7 +6,7 @@
 /*   By: ylagtab <ylagtab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/15 09:00:00 by ylagtab           #+#    #+#             */
-/*   Updated: 2021/01/15 19:21:58 by ylagtab          ###   ########.fr       */
+/*   Updated: 2021/02/20 16:40:12 by ylagtab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	ft_vector_realloc(t_vector *vector, size_t new_capacity)
 	size_t	old_size;
 	size_t	new_size;
 
-	old_size = vector->capacity * sizeof(t_element*);
+	old_size = vector->length * sizeof(t_element*);
 	new_size = new_capacity * sizeof(t_element*);
 	tmp = vector->array;
 	vector->array = (t_element**)ft_realloc(tmp, old_size, new_size);
